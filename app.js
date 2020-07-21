d9a1ddfc0805c89951a09c33896d2ff8226b2656
Zhifeng <tzephons@gmail.com>
2020-07-21 16:49:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var twitterRouter = require('./routes/twitter');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/twitter', twitterRouter);
+
+module.exports = app;
---
+++ routes/twitter.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var Sentiment = require('sentiment');
+var Twitter = require('twitter');
+var router = express.Router();
+var moment = require('moment');
+
+var sentiment = new Sentiment();
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var tw, arr = [];
+
+router.get('/:username', function (req, res, next) {
+    var params = { screen_name: req.params.username };
+    client.get('statuses/user_timeline', params, (error, tweets, response) => {
+        if (!error && tweets != null) {
+            tw = tweets;
+            arr = [];
+        }
+        tw.forEach(function (item) {
+            obj = {
+                'username': item.user.screen_name,
+                'tweet_text': item.text,
+                'created_at': moment(item.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'CET').format('DD/MM/YYYY, HH:mm A'),
+                'sentiment': sentiment.analyze(item.text)
+            };
+            arr.push(obj);
+        });
+        res.send(arr);
+    });
+});
+
+module.exports = router;
