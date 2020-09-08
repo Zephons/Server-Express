4e9aed6c0f7f04afc0c97a3ad7f67c39a7b76e01
Zhifeng <tzephons@gmail.com>
2020-09-09 00:38:15 +0200

    Cors origin.

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({origin: ['https://the-tweeth-sense.herokuapp.com', 'http://localhost:4200']}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/twitter', twitterRouter);
